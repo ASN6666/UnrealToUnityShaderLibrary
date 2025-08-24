6e3593589468a620f61309dd9050d945e95aa0b4
ASN6666 <74660180+ASN6666@users.noreply.github.com>
2025-08-25 02:37:00 +0800

    Add AdditionalLightFucntion

--- UnrealToUnityLibrary.hlsl
+++ UnrealToUnityLibrary.hlsl
@@ -1,6 +1,7 @@
 ﻿#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
 
-//Get Gradiegnt
+//Get Gradient
 float4 Gradient(float4 Color1, float4 Color2 , float offset,float2 uv)
  {
      float4 col = lerp(Color1,Color2, uv.y * offset );
@@ -13,4 +14,21 @@ float4 BaseCelShader(float4 MainLight,float3 VertexNormal,float4 ColorA,float4 C
     float4 col;
     col = saturate(step(Range,dot(MainLight,VertexNormal))+ ShadowPower);
     return col;
+}
+
+//AdditionalLightFucntion(Test)
+float3 AllAdditionalLightPass(float3 WorldPosition,float3 WorldNormal,float2 CutoffThreshold)
+{
+    float3 COLOR;
+    int lightcount  =GetAdditionalLightsCount();
+    for (int i = 0; i < lightcount; i++)
+    {
+        Light light = GetAdditionalLight(i,WorldPosition);
+        COLOR = dot(light.direction,WorldNormal);
+        COLOR = smoothstep(CutoffThreshold.x,CutoffThreshold.y,COLOR);
+        COLOR *= light.color;
+        COLOR *= light.distanceAttenuation;
+        
+    }
+    return COLOR;
 }
