6a69b98b1261f5e3ec7ce2d64abdb0005bf4eb4c
ASN6666 <74660180+ASN6666@users.noreply.github.com>
2025-08-27 18:47:08 +0800

    Add SDF Calculation

--- UnrealToUnityLibrary.hlsl
+++ UnrealToUnityLibrary.hlsl
@@ -34,6 +34,7 @@ void AllAdditionalLightPass(float3 WorldPosition,float3 WorldNormal,float2 Cutof
     {
         Light light = GetAdditionalLight(i,WorldPosition);
         COLOR = dot(light.direction,WorldNormal);
+        //COLOR = smoothstep(.4,.7,COLOR);
         COLOR = smoothstep(CutoffThreshold.x,CutoffThreshold.y,COLOR);
         COLOR *= light.color;
         COLOR *= light.distanceAttenuation;
@@ -55,12 +56,37 @@ float4 GetFresnel(float3 WorldPositon,float3 VertexNormal,float _min ,float _max
     rimLgihtArea_Dir  *= fresnel * float4((FrontColor),1);
     NrimLgihtArea_Dir *= fresnel * float4((BackColor),1);
     return NrimLgihtArea_Dir + rimLgihtArea_Dir;
-    return  rimLgihtArea_Dir  * fresnel * float4((FrontColor),1);
-   
+    //return  rimLgihtArea_Dir  * fresnel * float4((FrontColor),1);
+
+    
 }
 
 void GetOutline(float3 position,float3 normal,float outline,out float3 Result)
 {
     //vert
     Result = position.xyz + normal * outline;
+}
+
+void SetSDF(sampler2D _SDFTex,float2 uv,out float4 result)
+{
+    float3 _forwardVec = unity_ObjectToWorld._m02_m12_m22;
+    float3 _SideVec = unity_ObjectToWorld._m00_m10_m20;
+
+    //取得向上向量
+    float3 upVector = cross(_forwardVec, _SideVec);
+    //dot(陽光方向，向上方向) 
+    float3 LpU = dot(_MainLightPosition, upVector)/pow(length(upVector), 2) * upVector;
+    float3 LpHeadHorizon = _MainLightPosition - LpU;
+    float value = acos(dot(normalize(LpHeadHorizon), normalize(_SideVec)))/3.142;
+    float exposeRight = step(value, 0.5);
+    float valueR = pow(1 - value * 2, 3);
+    float valueL = pow(value * 2 - 1, 3);
+    float mixValue = lerp(valueL, valueR, exposeRight);
+    float sdfRembrandtLeft = tex2D(_SDFTex, float2(1 - uv.x, uv.y)).r;
+    float sdfRembrandtRight = tex2D(_SDFTex, uv).r;
+    float mixSdf = lerp(sdfRembrandtRight, sdfRembrandtRight, .5);
+                    
+    float sdf = step(mixValue, mixSdf);
+    sdf = lerp(0, sdf, step(0, dot(normalize(LpHeadHorizon), normalize(_forwardVec))));
+    result = sdf;
 }
